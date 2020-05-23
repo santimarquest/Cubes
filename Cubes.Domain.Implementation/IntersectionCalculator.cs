13237d8ba5070600cc6816aa3708d90e813bfa18
DESKTOP-9KGNBCR\SANTI <santimarquest@hotmail.com>
2020-05-23 14:54:11 +0200

    multithreading

--- Cubes.Domain.Implementation/IntersectionCalculator.cs
+++ Cubes.Domain.Implementation/IntersectionCalculator.cs
@@ -3,6 +3,7 @@ using Cubes.Domain.Contracts.Objects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Threading.Tasks;
 
 namespace Cubes.Domain.Implementation
@@ -14,10 +15,29 @@ namespace Cubes.Domain.Implementation
         // Se podría cambiar esta firma por:     
         // public asinc Task<bool> FindParallelCubeIntersection(Cube firstCube, Cube secondCube)
         // lo que permitiría hacer  var result = await Task.WhenAll(task1, task2, task3); para liberar el thread mientras se estan realizando los cálculos
-        // También vale este comentario para la siguiente función CalculateParallelCubeIntersectionFigure(
+        // También vale este comentario para la siguiente función CalculateParallelCubeIntersectionFigure
+        // Parece ser que esta firma solo es posible a partir de C# 8.0, y estamos trabajando con C# 7.3, he decidido no cambiar el 
+        // lenguaje
+
         public bool FindParallelCubeIntersection(Cube firstCube, Cube secondCube)
         {
-            // Una forma de ejecutar tareas en paralelo, esperar que acaben y obtener el resultado de las mismas
+            // Habría que ver si es mejor ejecutar 3 tareas secuenciales, pero podemos ejecutar solo 1 o 2 de ellas.
+            // O ejecutarlas siempre todas en paralelo, esperar que acaben todas, y buscar si alguna de ellas ha tenido como resultado false.
+            // Dejo la primera opción en comentarios encima de la segunda solución escogida.
+            // En este caso concreto los resultados son muy parecidos
+
+            //bool result = true;
+
+            //Task<bool> task1 = Task<bool>.Run(() => FindAxisIntersection(firstCube.Centre.Abscissa, firstCube.EdgeSize, secondCube.Centre.Abscissa, secondCube.EdgeSize));
+            //if (!task1.Result) return false;
+
+            //Task<bool> task2 = Task<bool>.Run(() => FindAxisIntersection(firstCube.Centre.Ordinate, firstCube.EdgeSize, secondCube.Centre.Ordinate, secondCube.EdgeSize));
+            //if (!task2.Result) return false;
+
+            //Task<bool> task3 = Task<bool>.Run(() => FindAxisIntersection(firstCube.Centre.Applicate, firstCube.EdgeSize, secondCube.Centre.Applicate, secondCube.EdgeSize));
+            //if (!task3.Result) return false;
+
+            // return result;
 
             var task1 = Task<bool>.Factory.StartNew(() => FindAxisIntersection(firstCube.Centre.Abscissa, firstCube.EdgeSize, secondCube.Centre.Abscissa, secondCube.EdgeSize));
             var task2 = Task<bool>.Factory.StartNew(() => FindAxisIntersection(firstCube.Centre.Ordinate, firstCube.EdgeSize, secondCube.Centre.Ordinate, secondCube.EdgeSize));
@@ -26,6 +46,8 @@ namespace Cubes.Domain.Implementation
             var result = Task.WhenAll(task1, task2, task3);
 
             return !result.Result.Contains(false);
+
+
         }
 
         public Ortoedro CalculateParallelCubeIntersectionFigure(Cube firstCube, Cube secondCube)
@@ -47,19 +69,16 @@ namespace Cubes.Domain.Implementation
             // El método mostrado en la función anterior es más sencillo, más corto y más claro.
 
             var dimensions = typeof(Point).GetProperties();
-            var listTask = new List<Task<decimal>>();
-            var listResult = new List<(Task<decimal>, string)>();
+            var listResult = new List<(decimal, string)>();
 
             foreach (var item in dimensions)
             {
                 var task = Task.Run(() => GetDimension((decimal)GetPropValue(firstCube.Centre, item.Name), firstCube.EdgeSize, (decimal)GetPropValue(secondCube.Centre, item.Name), secondCube.EdgeSize));
-                listTask.Add(task);
-
-                var tuple = (task, item.Name);
+                
+                // Esto tiene que evitarse! , task.Result es bloqueante para este thread.
+                var tuple = (task.Result, item.Name);
                 listResult.Add(tuple);
             }
-
-            var result = Task.WhenAll(listTask);
 
             // Necesario para respetar el orden de los parámetros del constructor para la clase ortoedro
 
@@ -67,7 +86,7 @@ namespace Cubes.Domain.Implementation
             var length = listResult.Where(x => x.Item2 == "Ordinate").FirstOrDefault().Item1;
             var depth = listResult.Where(x => x.Item2 == "Applicate").FirstOrDefault().Item1;
 
-            return new Ortoedro(width.Result, length.Result, depth.Result);
+            return new Ortoedro(width, length, depth);
         }
 
         #endregion .: Public Methods :.
